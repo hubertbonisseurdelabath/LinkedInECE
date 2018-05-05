7bf33ca49dda32f7dfbc819160ed04938dec0a69
hubertbonisseurdelabath <36474951+hubertbonisseurdelabath@users.noreply.github.com>
2018-05-05 09:24:41 +0200

    Update notif.php

--- notif.php
+++ notif.php
@@ -3,33 +3,50 @@ session_start();
 
 ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Header</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="jquery.min.js"></script>
+  <script src="../JS/bootstrap.min.js"></script>
+</head>
+<body>
 
-    <title>Mon réseau</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="offcanvas.css" rel="stylesheet">
-  </head>
-
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">LinkedECE</a>
+    </div>
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Accueil</a></li>
+        <li><a href="profil.php">Profil</a></li>
+        <li><a href="#">Réseau</a></li>
+        <li><a href="#">Emplois</a></li>
+        <li><a href="#">Notifications</a></li>
+        <li><a href="#">Album</a></li>
+    </ul>
+    <form class="navbar-right form-inline ">
+        <input class="form-control mr-sm-10" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Inscription </a></li>
+      <li><a href="connect.php"><span class="glyphicon glyphicon-log-in"></span> Connexion</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
   <body class="bg-light">
 
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-      <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
       <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+          </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
@@ -38,64 +55,39 @@ session_start();
       </div>
     </nav>
 
-    <div class="nav-scroller bg-white box-shadow">
-      <nav class="nav nav-underline">
-
-          <a class="nav-link" href="#">Amis
-          <span class="badge badge-pill bg-light align-text-bottom">27</span>
-        </a>
-
-        <a class="nav-link" href="#">Recherche</a>
-        <a class="nav-link" href="#">Suggestions</a>
-        <a class="nav-link" href="#">Demandes en attente</a>
-      </nav>
-    </div>
-
     <main role="main" class="container">
       <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded box-shadow">
         <img class="mr-3" src="https://getbootstrap.com/assets/brand/bootstrap-outline.svg" alt="" width="48" height="48">
         <div class="lh-100">
-          <h6 class="mb-0 text-white lh-100">Mon réseau</h6>
-          <small>LinkedECE</small>
+         <strong class="d-block text-gray-dark">Notifications</strong>
         </div>
       </div>
 
       <div class="my-3 p-3 bg-white rounded box-shadow">
-        <h6 class="border-bottom border-gray pb-2 mb-0">Suggestions</h6>
+        <h6 class="border-bottom border-gray pb-2 mb-0">Notifications récentes</h6>
         <div class="media text-muted pt-3">
           <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-          <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-            <div class="d-flex justify-content-between align-items-center w-100">
-              <strong class="text-gray-dark">Full Name</strong>
-              <a href="#">Ajouter</a>
-            </div>
-            <span class="d-block">@username</span>
-          </div>
+          <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+            <strong class="d-block text-gray-dark">@username</strong>
+                    contenu notif</p>
         </div>
         <div class="media text-muted pt-3">
-          <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-          <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-            <div class="d-flex justify-content-between align-items-center w-100">
-              <strong class="text-gray-dark">Full Name</strong>
-              <a href="#">Ajouter</a>
-            </div>
-            <span class="d-block">@username</span>
-          </div>
+          <img data-src="holder.js/32x32?theme=thumb&bg=e83e8c&fg=e83e8c&size=1" alt="" class="mr-2 rounded">
+          <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+            <strong class="d-block text-gray-dark">@username</strong>
+            contenu notif</p>
         </div>
         <div class="media text-muted pt-3">
-          <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-          <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-            <div class="d-flex justify-content-between align-items-center w-100">
-              <strong class="text-gray-dark">Full Name</strong>
-              <a href="#">Ajouter</a>
-            </div>
-            <span class="d-block">@username</span>
-          </div>
+          <img data-src="holder.js/32x32?theme=thumb&bg=6f42c1&fg=6f42c1&size=1" alt="" class="mr-2 rounded">
+          <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+            <strong class="d-block text-gray-dark">@username</strong>
+                contenu notif</p>
         </div>
         <small class="d-block text-right mt-3">
-          <a href="#">All suggestions</a>
+          <a href="#">Voir plus</a>
         </small>
       </div>
+
     </main>
 
     <!-- Bootstrap core JavaScript
@@ -107,5 +99,14 @@ session_start();
     <script src="../../dist/js/bootstrap.min.js"></script>
     <script src="../../assets/js/vendor/holder.min.js"></script>
     <script src="offcanvas.js"></script>
-  </body>
+      
+</body>
+    <footer class="my-5 pt-5 text-muted text-center text-small">
+        <p class="mb-1">&copy; 2017-2018 ECE Student's</p>
+        <ul class="list-inline">
+          <li class="list-inline-item"><a href="#">Privacy</a></li>
+          <li class="list-inline-item"><a href="#">Terms</a></li>
+          <li class="list-inline-item"><a href="#">Support</a></li>
+        </ul>
+      </footer>
 </html>
